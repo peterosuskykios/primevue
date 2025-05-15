8b9e38a8194b65b4516da2d25cfbe52af37a8e86
Peter Osusky <peter.osusky@kios.sk>
2025-05-15 09:39:02 +0200

    deleted paths from index.js

--- src/router/index.js
+++ src/router/index.js
@@ -17,14 +17,6 @@ const router = createRouter({
                     component: () => import('@/views/dashboards/Ecommerce.vue')
                 },
                 {
-                    path: '/dashboard-banking',
-                    name: 'dashboard-banking',
-                    meta: {
-                        breadcrumb: ['Banking Dashboard']
-                    },
-                    component: () => import('@/views/dashboards/Banking.vue')
-                },
-                {
                     path: '/pages/empty',
                     name: 'empty',
                     component: () => import('@/views/pages/Empty.vue')
@@ -33,62 +25,6 @@ const router = createRouter({
                     path: '/pages/crud',
                     name: 'crud',
                     component: () => import('@/views/pages/Crud.vue')
-                },
-                {
-                    path: '/ecommerce/product-overview',
-                    name: 'product-overview',
-                    meta: {
-                        breadcrumb: ['E-Commerce', 'Product Overview']
-                    },
-                    component: () => import('@/views/e-commerce/ProductOverview.vue')
-                },
-                {
-                    path: '/ecommerce/product-list',
-                    name: 'product-list',
-                    meta: {
-                        breadcrumb: ['E-Commerce', 'Product List']
-                    },
-                    component: () => import('@/views/e-commerce/ProductList.vue')
-                },
-                {
-                    path: '/ecommerce/new-product',
-                    name: 'new-product',
-                    meta: {
-                        breadcrumb: ['E-Commerce', 'New Product']
-                    },
-                    component: () => import('@/views/e-commerce/NewProduct.vue')
-                },
-                {
-                    path: '/ecommerce/shopping-cart',
-                    name: 'shopping-cart',
-                    meta: {
-                        breadcrumb: ['E-Commerce', 'Shopping Cart']
-                    },
-                    component: () => import('@/views/e-commerce/ShoppingCart.vue')
-                },
-                {
-                    path: '/ecommerce/checkout-form',
-                    name: 'checkout-form',
-                    meta: {
-                        breadcrumb: ['E-Commerce', 'Checkout Form']
-                    },
-                    component: () => import('@/views/e-commerce/CheckoutForm.vue')
-                },
-                {
-                    path: '/ecommerce/order-history',
-                    name: 'order-history',
-                    meta: {
-                        breadcrumb: ['E-Commerce', 'Order History']
-                    },
-                    component: () => import('@/views/e-commerce/OrderHistory.vue')
-                },
-                {
-                    path: '/ecommerce/order-summary',
-                    name: 'order-summary',
-                    meta: {
-                        breadcrumb: ['E-Commerce', 'Order Summary']
-                    },
-                    component: () => import('@/views/e-commerce/OrderSummary.vue')
                 },
                 {
                     path: '/cms/stranky',
@@ -105,37 +41,6 @@ const router = createRouter({
                         breadcrumb: ['CMS', 'Subory']
                     },
                     component: () => import('@/views/user-management/UserList.vue')
-                },
-                {
-                    path: '/pages/aboutus',
-                    name: 'aboutus',
-                    meta: {
-                        breadcrumb: ['Pages', 'About']
-                    },
-                    component: () => import('@/views/pages/AboutUs.vue')
-                },
-                {
-                    path: '/pages/contact',
-                    name: 'contact',
-                    component: () => import('@/views/pages/ContactUs.vue')
-                },
-                {
-                    path: '/pages/faq',
-                    name: 'faq',
-                    meta: {
-                        breadcrumb: ['Pages', 'FAQ']
-                    },
-                    component: () => import('@/views/pages/Faq.vue')
-                },
-                {
-                    path: '/pages/help',
-                    name: 'help',
-                    component: () => import('@/views/pages/Help.vue')
-                },
-                {
-                    path: '/pages/invoice',
-                    name: 'invoice',
-                    component: () => import('@/views/pages/Invoice.vue')
                 }
             ]
         },
