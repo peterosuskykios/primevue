ef1d7cb77d23007fdae3f0d705e0abdb0bdb4604
Peter Osusky <peter.osusky@kios.sk>
2025-05-15 14:11:11 +0200

    repair imports

--- modules/main.js
+++ modules/main.js
@@ -2,13 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './base/router';
 
-import BlockViewer from '@/base/BlockViewer.vue';
+import BlockViewer from './base/BlockViewer.vue';
 import Aura from '@primeuix/themes/aura';
 import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
 
-import '@/assets/styles.scss';
+import './assets/styles.scss';
 
 const app = createApp(App);
 
